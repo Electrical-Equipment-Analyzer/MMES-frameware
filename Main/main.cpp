0eb3af0ba92c67a9fb84addadd9cf6e4d2581f11
薛聿明 <D10307009@mail.ntust.edu.tw>
2015-03-04 19:24:35 +0800

    Test SPI Flash OK.

--- Main/main.cpp
+++ Main/main.cpp
@@ -19,6 +19,9 @@ ConfigFile cfg;
 #include "SDFileSystem.h"
 SDFileSystem sd(p5, p6, p7, P2_2, "sd"); // the pinout on the mbed Cool Components workshop board
 
+
+#include "at45db161d.h"
+
 DigitalOut led2(P0_22);
 void led2_thread(void const *args) {
     while (true) {
@@ -80,36 +83,102 @@ void test_sdcf() {
     }
 }
 
-I2C i2c(p28, p27);
-const int addr = 0xA0;
+
+#undef PAGE_SIZE
+#define PAGE_SIZE 264 // AT45DB081D (1MB)
+//#define PAGE_SIZE 528 // AT45DB321D (4MB)
+#define PAGE_NUM 4095 // AT45DB081D (1MB)
+//#define PAGE_NUM 8192 // AT45DB321D (4MB)
+
+#define WRITE_BUFFER 1
+#define READ_BUFFER 2
+
+SPI spi(p5, p6, p7); // mosi, miso, sclk
+ATD45DB161D memory(spi, P2_2);
+
+void flash_write (int addr, char *buf, int len) {
+    int i;
+    memory.BufferWrite(WRITE_BUFFER, addr % PAGE_SIZE);
+    for (i = 0; i < len; i ++) {
+        spi.write(buf[i]);
+    }
+    memory.BufferToPage(WRITE_BUFFER, addr / PAGE_SIZE, 1);
+}
+
+void flash_read (int addr, char *buf, int len) {
+    int i;
+    memory.PageToBuffer(addr / PAGE_SIZE, READ_BUFFER);
+    memory.BufferRead(READ_BUFFER, addr % PAGE_SIZE, 1);
+    for (i = 0; i < len; i ++) {
+        buf[i] = spi.write(0xff);
+    }
+}
 
 void test_rom() {
-    pc.printf("rom\r\n");
+    int i;
+    char buf[PAGE_SIZE];
+    Timer t;
+    ATD45DB161D::ID id;
 
+    spi.frequency(10000000);
+    wait_ms(500);
 
-    char data[10];
-    data[0] = 0x00;
-    data[1] = 0x00;
-    data[2] = 0x12;
-    data[3] = 0x34;
-    data[4] = 0x56;
-    data[5] = 0x78;
-    data[6] = 0x90;
-    data[7] = 0xAA;
-    data[8] = 0xBB;
-    data[9] = 0xCC;
-    data[10] = 0xDD;
-    i2c.write(addr, data, 10);
+    memory.ReadManufacturerAndDeviceID(&id);
+    pc.printf("RAM Manufacturer ID : %02x\r\n", id.manufacturer);
+    pc.printf("RAM Device ID : %02x %02x\r\n", id.device[0], id.device[1]);
+    wait_ms(10);
 
-    char cmd[2];
-    cmd[0] = 0x00;
-    cmd[1] = 0x00;
-    i2c.write(addr, cmd, 2);
-    uint8_t i = 0;
-    for (i = 0; i < 10; i++) {
-        i2c.read(addr | 0x01, cmd, 1);
-        pc.printf("%x\r\n", cmd[0]);
+    pc.printf("\r\nHELLO test\r\n");
+
+    pc.printf("RAM write\r\n");
+    strcpy(buf, "Hello!");
+    flash_write(0, buf, 6);
+    for (i = 0; i < PAGE_SIZE; i ++) {
+        buf[i] = i;
     }
+    flash_write(6, buf, PAGE_SIZE - 6);
+
+    wait(1);
+    memset(buf, 0, PAGE_SIZE);
+
+    pc.printf("RAM read\r\n");
+    flash_read(0, buf, PAGE_SIZE);
+    for (i = 0; i < PAGE_SIZE; i ++) {
+        pc.printf(" %02x", buf[i]);
+        if ((i & 0x0f) == 0x0f)
+            pc.printf("\r\n");
+    }
+
+    wait(1);
+
+    pc.printf("\r\nWrite/Read time\r\n");
+
+    pc.printf("RAM write\r\n");
+    t.reset();
+    t.start();
+    for (i = 0; i < 0x20000; i += PAGE_SIZE) {
+        buf[0] = (i >> 8) & 0xff;
+        flash_write(i, buf, PAGE_SIZE);
+        if ((i & 0x0fff) == 0) printf(".");
+    }
+    t.stop();
+    pc.printf("\r\ntime %f, %f KBytes/sec\r\n", t.read(), (float)0x20000 / 1024 / t.read());
+
+    wait(1);
+
+    pc.printf("RAM read\r\n");
+    t.reset();
+    t.start();
+    for (i = 0; i < 0x20000; i += PAGE_SIZE) {
+        flash_read(i, buf, PAGE_SIZE);
+        if (buf[0] != ((i >> 8) & 0xff)) {
+            pc.printf("error %d\r\n", i);
+            break;
+        }
+        if ((i & 0x0fff) == 0) printf(".");
+    }
+    t.stop();
+    pc.printf("\r\ntime %f, %f KBytes/sec\r\n", t.read(), 0x20000 / 1024 / t.read());
 }
 
 int main() {
